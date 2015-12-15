10c0542116195ef5862f873064ab89f72fd0adc7
ligadata-ramana <ramana@ligadata.com>
2015-12-14 17:56:22 -0800

    Hashmap issues concurrent kafka client threads accessing same table when the engine starting up

--- trunk/Storage/HashMap/src/main/scala/com/ligadata/keyvaluestore/HashMapAdapter.scala
+++ trunk/Storage/HashMap/src/main/scala/com/ligadata/keyvaluestore/HashMapAdapter.scala
@@ -194,11 +194,15 @@ class HashMapAdapter(val kvManagerLoader: KamanjaLoaderInfo, val datastoreConfig
   }
 
   private def CheckTableExists(containerName: String): Unit = {
+    logger.debug("CheckTableExists: " + containerName + ",this => " + this)
     if (containerList.contains(containerName)) {
       return
-    } else {
-      CreateContainer(containerName)
-      containerList.add(containerName)
+    }
+    lock.synchronized {
+      if (containerList.contains(containerName) == false) {
+        CreateContainer(containerName)
+        containerList.add(containerName)
+      }
     }
   }
 
